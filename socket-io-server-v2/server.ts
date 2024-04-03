d7d407b31112e5bd941bc405a3abf12d08faa2fc
OPALKUUUBDEV <makara.atipatipat.dev@gmail.com>
2024-04-03 15:59:04 +0700

    [add][socket] - add action

--- socket-io-server-v2/server.ts
+++ socket-io-server-v2/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import http from 'http';
 import { Server, Socket } from 'socket.io';
 import routes from './routes'
+import { INITIAL_DATA } from './configs/socketio';
 
 export const app: Application = express()
 
@@ -17,14 +18,75 @@ app.use('/api', routes)
 const server = http.createServer(app);
 const io = new Server(server);
 
+let time = 0;
+let data = INITIAL_DATA
 io.on('connection', (socket: Socket) => {
-    console.log('A user connected');
+  
+    console.log(`UserId[${socket.id}] Connected...`);
+
+    socket.emit("initial-data", data);
+
+    socket.emit("time", time);
+
+    socket.on("time", (updateTime) => {
+      time = updateTime;
+      io.emit("time", time);
+    });
+
+    socket.on("update-score", (id, newScore) => {
+      const player = data.findIndex((player) => player.id === id);
+      if (player !== -1) {
+        data[player] = {
+          ...data[player],
+          score: newScore,
+        };
+        io.emit("updated-data", data);
+      }
+    });
+
+    socket.on("update-question", (id, newQuestion) => {
+      const player = data.findIndex((player) => player.id === id);
+      if (player !== -1) {
+        data[player] = {
+          ...data[player],
+          question: newQuestion,
+        };
+        io.emit("updated-data", data);
+      }
+    });
+
+    socket.on("update-isIncorrect", (id, NewisIncorrect) => {
+      const player = data.findIndex((player) => player.id === id);
+      if (player !== -1) {
+        data[player] = {
+          ...data[player],
+          isIncorrect: NewisIncorrect,
+        };
+        io.emit("updated-data", data);
+      }
+    });
+
+    socket.on("update-disabledInput", (id, NewDisabledInput) => {
+      const player = data.findIndex((player) => player.id === id);
+      if (player !== -1) {
+        data[player] = {
+          ...data[player],
+          disabledInput: NewDisabledInput,
+        };
+        io.emit("updated-data", data);
+      }
+    });
+  
+    socket.on("message", (message) => {
+      io.emit("message", message);
+    });
+
     socket.on('disconnect', () => {
         console.log('A user disconnected');
     });
 });
 
-const PORT = 8080
+const PORT = 3001
 
 try {
   server.listen(PORT, (): void => {
